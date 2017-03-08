9dfc18d721c360dbc5e6df0190ae4fa2cc74efaf
直登 木村 <e36358@jp.ibm.com>
2017-03-07 23:33:59 -0600

    cloudant_test

--- app.js
+++ app.js
@@ -21,6 +21,84 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+
+/***ここから追加***/
+// POSTパラメータ取得用 body-parser設定 (express4から必要)
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Date()で現在時刻を取得するためのユーティリティ
+var dateutil = require('date-utils');
+
+// Cloudant用アクセス・モジュール「cradle」設定
+var cradle = require('cradle');
+
+// Cloudant DB接続情報取得
+var services = JSON.parse(process.env.VCAP_SERVICES);
+var credentials = services['cloudantNoSQLDB'][0].credentials;
+var host = credentials.host;
+var port = credentials.port;
+var options = {
+	cache : true,
+	raw : false,
+	secure : true,
+	auth : {
+		username : credentials.username,
+		password : credentials.password
+	}
+};
+
+// データベース接続・cldbを指定
+var db = new (cradle.Connection)(host, port, options).database('cldb');
+
+//追加ボタン押された時にdbに追加する
+app.post('/', function(req, res){
+	var date = new Date();
+	var now = date.toFormat("YYYY/MM/DD HH24:MI:SS");
+	req.body.date = now;
+	// 項目の保存
+	db.save(now, req.body);
+	res.send(req.body);
+});
+
+//「全件削除」ボタンの id=removeAll, ui_item.jsの url:'/removeAll'でcall
+app.post('/removeAll', function(req, res){
+	// 全件検索を、作成したview名 items_view にて実行
+	db.view('items/items_view', function (err, rows) {
+		if (!err) {
+			rows.forEach(function (id, row) {
+				db.remove(id);
+				console.log("removed key is: %s", id);
+			});
+		} 
+		else { console.log("app.js db.remove error: " + err); }
+	});
+	res.send({});
+});
+
+
+//「全件表示」ボタンの id=getAll, ui_item.jsの url:'/getAll'でcall
+app.post('/getAll', function(req, res){
+	returnTable(res);
+});
+
+var returnTable = function(res) {
+	// 全件検索を、作成したview名 items_view にて実行
+	db.view('items/items_view', function (err, rows) {
+		if (!err) {
+			rows.forEach(function (id, row) {
+				console.log("key: %s, row: %s", id, JSON.stringify(row));
+			});
+		} 
+		else 
+		{ console.log("app.js returnTable error: " + err); }
+		res.send(rows);
+	});
+}
+/***ここまで追加***/
+
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
---
+++ public/js/ui_table.js
@@ -0,0 +1,68 @@
+// ui_table.js ブラウザUI用 JavaScript (index.htmlより呼ばれる)
+
+$(function(){
+ 	console.log('ui_item.js in');
+ 	// サーバから取得したデータを、htmlテーブルに追加
+ 	var showTable = function(data) {
+ 		$("#tableItems").append("<tr></tr>")
+ 		.find("tr:last")
+ 		.append("<td>" + data.date + "</td>")
+ 		.append("<td>" + data.item1 + "</td>")
+ 	};
+ 	
+ 	// 追加ボタン（index.htmlのid=add）押下時 実行
+ 	$("#add").click(function(e){
+ 		e.preventDefault();
+ 		var param = {};
+ 		param.item1 = $("#item1").val() || "";
+ 		// POSTでのajaxコールで、サーバーのapp.jsのapp.post /add呼び出し
+ 		$.ajax({
+ 			type: 'POST',
+ 			data: JSON.stringify(param),
+ 			contentType: 'application/json',
+ 			url: '/',
+ 			success: function(data) {
+ 				console.log('success add: ' + JSON.stringify(data));
+ 				showTable(data);
+ 			},
+ 			error: function(data) { console.log('error add: ' + JSON.stringify(data)); }
+ 		});
+ 		// 入力項目名を空白に
+ 		$("#item1").val('');
+ 	});
+
+ 	// 全件表示ボタン（index.htmlのid=getAll）押下時 実行
+ 	$("#getAll").click(function(e){
+ 		e.preventDefault();
+		$("#tableItems").empty();
+ 		// POSTでのajaxコールで、サーバーのapp.jsのapp.post /getAll呼び出し
+		$.ajax({
+			type: 'POST',
+			data: {},
+			contentType: 'application/json',
+			url: '/getAll',
+			success: function(rows) {
+				for(var i=0; i<rows.length; i++) {
+					console.log(' row '+ i +": "+ JSON.stringify(rows[i]));
+					showTable(rows[i].value);
+				}
+			},
+			error: function(data) { console.log('error getAll: ' + JSON.stringify(data)); }
+		});
+ 	});
+
+	// 全件削除ボタン（index.htmlのid=removeAll）押下時 実行
+	$("#removeAll").click(function(e){
+		e.preventDefault();
+		// POSTでのajaxコールで、サーバーのapp.jsのapp.post呼び出し
+		$.ajax({
+			type: 'POST',
+			data: {},
+			contentType: 'application/json',
+			url: '/removeAll',
+			success: function(data) { console.log('success removeAll'); },
+			error: function(data) { console.log('error getAll: ' + JSON.stringify(data)); }
+		});
+		$("#tableItems").empty();
+	});
+ });
